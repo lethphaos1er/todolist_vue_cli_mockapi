a06bbab8efaab17a1c9724e5ccde102fb9fb7313
Cedric Catoul <catoul.cedric@protonmail.com>
2025-11-17 15:10:06 +0100

    architecture des components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
-<script setup></script>
+<script setup>
+import AppHeader from '@/components/AppHeader.vue';
+import AppFooter from '@/components/AppFooter.vue';
+import TodoList from '@/components/todoList/TodoList.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <AppHeader />
+  <main>
+    <TodoList />
+  </main>
+  <AppFooter />
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
---
+++ src/components/todoList/TodoListFooter.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+<template>
+<!-- FOOTER DE LISTE -->
+        <div
+          class="border-t border-slate-300 bg-slate-300 px-4 py-3 sm:px-5 text-slate-600"
+          role="contentinfo"
+          aria-label="Todo list footer"
+        >
+          <div class="flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-4">
+            <!-- Compteur avec aria-live -->
+            <p class="text-sm">
+              <span
+                id="items-left"
+                class="font-medium"
+                aria-live="polite"
+                aria-atomic="true"
+                >2</span
+              >
+              items left
+            </p>
+          </div>
+        </div>
+</template>
+<style scoped></style>
